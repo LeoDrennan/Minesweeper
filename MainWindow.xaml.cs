eb932f50567ecc7d8e7e19fc4a08dde29b577053
Leo Drennan <ldrennan@softwareinstitute.com>
2023-05-04 09:12:23 +0100

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,7 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static System.Reflection.Metadata.BlobBuilder;
 
-namespace WpfApp3
+namespace Minesweeper
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,40 +26,19 @@ namespace WpfApp3
     {
         public MainWindow()
         {
-            const int columns = 16;
-            const int rows = 16;
-            const int mines = 40;
-
+            // Window and grid size declaration
+            const int columns = 10;
+            const int rows = 10;
+            const int mines = 10;
             Application.Current.MainWindow.Height = 35 * rows + 70;
             Application.Current.MainWindow.Width = 35 * columns;
 
             InitializeComponent();
 
-            createGrid(rows, columns);
+
             char[,] minefield = populateMinefield(rows, columns, mines);
-            createButtons(rows, columns, minefield);
-        }
-
-        public void createGrid(int rows, int columns)
-        {
-            mineGrid minesweeper = new mineGrid();
-            minesweeper.HorizontalAlignment = HorizontalAlignment.Center;
-            minesweeper.VerticalAlignment = VerticalAlignment.Center;
-
-            for (int i = 0; i < rows; i++)
-            {
-                RowDefinition rowDefinition = new RowDefinition();
-                rowDefinition.Height = new GridLength(30);
-                this.minesweeper.RowDefinitions.Add(rowDefinition);
-            }
-
-            for (int i = 0; i < columns; i++)
-            {
-                ColumnDefinition columnDefinition = new ColumnDefinition();
-                columnDefinition.Width = new GridLength(30);
-                this.minesweeper.ColumnDefinitions.Add(columnDefinition);
-
-            }
+            gridButton[,] gameButtons = createButtons(rows, columns, minefield);
+            createGrid(rows, columns, gameButtons);
         }
 
         public char[,] populateMinefield(int rows, int columns, int bombs)
@@ -91,44 +72,6 @@ namespace WpfApp3
             return mineGrid;
         }
 
-        public void createButtons(int rows, int columns, char[,] minefield)
-        {
-            int count = 1;
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    gridButton btn = new gridButton();
-                    btn.Name = "button" + count.ToString();
-                    btn.Click += buttonClick;
-                    btn.MouseRightButtonUp += rightClick;
-                    btn.pressed = false;
-                    btn.isFlag = false;
-                    btn.Content = '\0';
-                    btn.value = minefield[i,j];
-
-                    switch (btn.value)
-                    {
-                        case '*':
-                            btn.type = "mine";
-                            break;
-                        case '0':
-                            btn.type = "zero";
-                            break;
-                        default:
-                            btn.type = "number";
-                            break;
-
-                    }
-                    Grid.SetColumn(btn, j);
-                    Grid.SetRow(btn, i);
-                    minesweeper.Children.Add(btn);
-                    count++;
-                }
-            }
-        }
-
         public char[,] neighbouringMines(char[,] mineGrid, int row, int column, int totalRows, int totalColumns)
         {
             int mineCount = 0;
@@ -136,7 +79,7 @@ namespace WpfApp3
             {
                 for (int j = column - 1; j <= column + 1; j++)
                 {
-                    if (i >= 0 && i < totalRows && j >= 0 && j < totalColumns && mineGrid[i,j] == '*')
+                    if (i >= 0 && i < totalRows && j >= 0 && j < totalColumns && mineGrid[i, j] == '*')
                     {
                         mineCount++;
                     }
@@ -156,20 +99,93 @@ namespace WpfApp3
             return mineGrid;
         }
 
+        public gridButton[,] createButtons(int rows, int columns, char[,] minefield)
+        {
+            int count = 1;
+            gridButton[,] gameButtons = new gridButton[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    gridButton btn = new gridButton();
+                    btn.xLoc = i;
+                    btn.yLoc = j;
+                    btn.Name = "button" + count.ToString();
+                    btn.Click += buttonClick;
+                    btn.MouseRightButtonUp += rightClick;
+                    btn.pressed = false;
+                    btn.isFlag = false;
+                    btn.Content = '\0';
+                    btn.hiddenContent = minefield[i, j];
+
+                    switch (btn.hiddenContent)
+                    {
+                        case '*':
+                            btn.type = "mine";
+                            break;
+                        case '0':
+                            btn.type = "zero";
+                            break;
+                        default:
+                            btn.type = "number";
+                            break;
+                    }
+
+                    Grid.SetColumn(btn, j);
+                    Grid.SetRow(btn, i);
+                    minesweeper.Children.Add(btn);
+                    gameButtons[i, j] = btn;
+                    count++;
+                }
+            }
+
+            return gameButtons;
+        }
+
+        public void createGrid(int rows, int columns, gridButton[,] minefield)
+        {
+            mineGrid minesweeper = new mineGrid();
+            minesweeper.rows = rows;
+            minesweeper.columns = columns;
+            minesweeper.minefieldButtons = minefield;
+
+            minesweeper.HorizontalAlignment = HorizontalAlignment.Center;
+            minesweeper.VerticalAlignment = VerticalAlignment.Center;
+
+            for (int i = 0; i < rows; i++)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = new GridLength(30);
+                this.minesweeper.RowDefinitions.Add(rowDefinition);
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                columnDefinition.Width = new GridLength(30);
+                this.minesweeper.ColumnDefinitions.Add(columnDefinition);
+
+            }
+        }
+
         public void buttonClick(object sender, RoutedEventArgs e)
         {
             gridButton pressedButton = sender as gridButton;
-            if (!pressedButton.isFlag && pressedButton.type != "mine")
+            if (!pressedButton.isFlag)
             {
                 pressedButton.pressed = true;
-                pressedButton.revealIdentity(pressedButton.pressed);
+                if (pressedButton.type != "*")
+                {
+                    pressedButton.revealIdentity();
+                }
+                else
+                {
+                    mineGrid currentGame = sender as mineGrid;
+                    currentGame.showAllMines();
+                }
             }
-            else if (!pressedButton.isFlag && pressedButton.type == "mine")
-            {
-                pressedButton.pressed = true;
-                pressedButton.revealIdentity(pressedButton.pressed);
-
-            }
+            
         }
 
         public void rightClick(object sender, RoutedEventArgs e)
@@ -187,31 +203,32 @@ namespace WpfApp3
             }
         }
 
-        private void resetClick(object sender, RoutedEventArgs e)
+        public class gridButton : Button
         {
-
-        }
-
-        class gridButton : Button
-        {
-            public char value;
+            public int xLoc;
+            public int yLoc;
+            public char hiddenContent;
             public bool pressed;
             public bool isFlag;
             public string type;
+            string flagEmoji = "\uD83D" + "\uDEA9";
 
-            string iconName = "\uD83D" + "\uDEA9";
-
-            public void revealIdentity(bool pressed)
+            public void revealIdentity()
             {
-                if (pressed && this.value != '*')
+                if (this.hiddenContent != '*')
                 {
                     this.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#eff3ba");
-                    this.Content = this.value;
+                    this.Content = this.hiddenContent;
                 }
-                else if (pressed && this.value == '*')
+                else if (this.hiddenContent == ' ')
+                {
+                    this.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#eff3ba");
+                }
+                else if (this.hiddenContent == '*')
                 {
                     this.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF0000");
-                    this.Content = this.value;
+                    this.Content = this.hiddenContent;
+
                 }
                 return;
             }
@@ -221,7 +238,7 @@ namespace WpfApp3
                 switch (isFlag)
                 {
                     case true:
-                        this.Content = iconName;
+                        this.Content = flagEmoji;
                         break;
                     case false:
                         this.Content = ' ';
@@ -230,9 +247,27 @@ namespace WpfApp3
             }
         }
 
-        class mineGrid : Grid
+        public class mineGrid : Grid
         {
-            
+            public int rows;
+            public int columns;
+            public gridButton[,] minefieldButtons;
+
+            public void showAllMines()
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (minefieldButtons[i,j].type == "mine")
+                        {
+                            minefieldButtons[i, j].Content = minefieldButtons[i, j].hiddenContent;
+                            minefieldButtons[i, j].Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF0000");
+                        }
+                    }
+                }
+                return;
+            }
         }
     }
 }
